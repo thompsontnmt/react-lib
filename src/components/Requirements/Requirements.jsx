6dad55d020e40fff97563224c80d11394bb465f3
Mike Thompson <mtthompson11@gmail.com>
2023-01-20 14:14:54 -0600

    new component

--- src/components/Requirements/Requirements.jsx
+++ src/components/Requirements/Requirements.jsx
@@ -1,3 +1,26 @@
-export const Requirements = () => {
-    return (<>Hello World</>);
-}
+import '../../styles.css';
+import { Requirement } from '../Requirement';
+import React, { useCallback, useEffect } from 'react';
+
+export const Requirements = ({
+    value,
+    requirements,
+    onValidChange,
+  }) => {
+  const validChangeCb = useCallback(onValidChange, []);
+
+  useEffect(() => {
+    validChangeCb(
+      requirements.every(r => r.validator(value))
+    );
+  }, [value, requirements, validChangeCb]);
+
+  return requirements.map((r, index) => (
+    <Requirement
+      key={index}
+      value={value}
+      requirement={r}
+      onValidChange={onValidChange}
+    />
+  ));
+};
